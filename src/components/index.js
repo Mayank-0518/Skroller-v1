ed565e2604c723fc31dd6de64db84c286cc35e4e
Mayank Kumar Maurya <23cs3038@rgipt.ac.in>
2024-12-25 18:57:11 +0530

    directory issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 import Header from "./header/Header";
-import Footer from "./footer/footer";
+import Footer from "./footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./header/LogoutBtn";
 import Container from "./container/Container";
